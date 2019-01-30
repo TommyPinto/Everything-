f7127030697e350534ea10bfadca3691bb3b9f5e
TommyPinto <tpinto@stevens.edu>
2019-01-29 19:49:48 -0500

    Homework is complete

--- hw0/hypot.cc
+++ hw0/hypot.cc
@@ -1,10 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
 int main() {
 	double a,b;
 	cout << "Enter a,b: ";
 	cin >> a >> b;
+double c = pow((a*a +b*b),.5);
+cout << "The hypotenuse is: " << c << endl;
 
+double area;
+area = .5 * a * b;
 
-
+cout <<"The area of this triangle is: " << area << endl;
 
 	return 0;
 }
