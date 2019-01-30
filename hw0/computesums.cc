9c5f4025f3e5c853a79af73431bfd7ec7d373e82
TommyPinto <tpinto@stevens.edu>
2019-01-29 19:39:53 -0500

    Homework is Completed

--- hw0/computesums.cc
+++ hw0/computesums.cc
@@ -1,18 +1,30 @@
 // sum the numbers from 1 to n and return the answer
+#include <iostream>
+using namespace std;
 int sum(int n) {
+	int total = 0;
+for(int i=0;i<=n;i++ )
+total+= i;
 
-
+return total;
 }
 
 // sum the numbers from 1/1 to 1/n and return
 float sumReciprocals(int n) {
+float total2 = 0;
+for (float i = 1; i <= n; i++)
+total2+=1/i;
 
-	
+return total2;
 }
 
 // sum the numbers from 1/n to 1/1 (same as above but reversed) and return
 float sumReciprocalsReverse(int n) {
+ float total3 = 0;
+ for (float i=n; i>=1; i--)
+ total3+=1/i;
 
+ return total3;
 }
 
 int main() {
