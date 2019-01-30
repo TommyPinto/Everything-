9b93d34de68a0335b6c9ac04d9d518f0c05a0c78
TommyPinto <tpinto@stevens.edu>
2019-01-29 19:23:48 -0500

    First Homework Completed

--- hw0/first.cc
+++ hw0/first.cc
@@ -2,10 +2,22 @@
 using namespace std;
 
 int main() {
+cout << "\nFirst Loop:\n";
+for (int i = 0; i <= 10; i++)
+cout << i << " ";
+cout << "\nSecond Loop:\n";
+for (int i = 10; i >= 0; i--)
+cout << i << " ";
+cout << "\nThird Loop:\n";
+for (int i = 0; i <= 10; i= i+2)
+cout << i << " ";
+
+cout << "\nFourth Loop:\n";
+
+for (int i = 1; i <= 1024; i= i*2)
+cout << i << " ";
 
 
-
-	
 
 	return 0;
 }
