f7aad384a2f7db514ac190c4aa419e800cddc4c3
TommyPinto <tpinto@stevens.edu>
2019-02-05 11:44:39 -0500

    360files

---
+++ 360stuff/firstcoding.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+int x;
+int y = x+10;
+cout << "Enter a number" << x << endl;
+cin >> x;
+cout << "This is your number plus 10"	<< y << endl;
+
+}
